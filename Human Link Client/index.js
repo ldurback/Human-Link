545e9dc1b19512393d1285391f6e6c5ab5c26058
Luke Durback <luke.durback@gmail.com>
2018-06-20 20:52:25 -0400

    Clean up

--- Human Link Client/index.js
+++
@@ -1,7 +0,0 @@
-// initialize the controller
-var Controller = require('./controller');
-var controller = new Controller();
-
-// start the ui
-controller.uiHost.serve("/ui/index.html");
-controller.uiHost.find_port_and_listen();
